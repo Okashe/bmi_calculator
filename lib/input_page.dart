cfdd305bda58f63d3f97b5cc0175d3a97d6075c5
Okashe <okashabdi88@gmail.com>
2023-05-30 20:43:34 -0500

    container used repetitively with expanded to get the look

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -11,20 +11,71 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('BMI CALCULATOR'),
+        appBar: AppBar(
+          title: const Center(
+            child: Text('BMI CALCULATOR'),
+          ),
         ),
-      ),
-      body: Container(
-        height: 200.0,
-        width: 170.0,
-        margin: const EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: const Color(0xff1d1e33),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xff1d1e33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1d1e33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
