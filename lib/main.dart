666a25ee81891ffe3cd60dfe47ca91f8246158c3
Okashe <okashabdi88@gmail.com>
2023-05-30 19:30:06 -0500

    theme modified & input_page and main separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(const BMICalculator());
@@ -11,49 +12,17 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: const ColorScheme(
-          primary: Colors.red,
-          brightness: Brightness.light,
-          onPrimary: Colors.white,
-          secondary: Colors.purple,
-          onSecondary: Colors.white,
-          error: Colors.white,
-          onError: Colors.red,
-          background: Colors.white,
-          onBackground: Colors.black,
-          surface: Color.fromARGB(19, 186, 254, 254),
-          onSurface: Color.fromARGB(6, 9, 0, 90),
+      theme: ThemeData.dark().copyWith(
+        colorScheme: const ColorScheme.light().copyWith(
+          primary: const Color(0xFF0A0E21),
+        ),
+        //secondary:Colors.yellow,
+        scaffoldBackgroundColor: const Color(0XFF0A0E21),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
         ),
       ),
       home: const InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('BMI CALCULATOR'),
-        ),
-      ),
-      body: const Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
